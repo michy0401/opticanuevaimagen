d6a7468912e4442a2d99f87661dcfec4fd23ea65
Michelle Argueta <michelleargueta401@gmail.com>
2025-07-31 10:12:46 -0600

    schemas cart

--- lib/validators.ts
+++ lib/validators.ts
@@ -40,3 +40,24 @@ export const signUpFormScheme = z.object({
     message: "Passwords don't match",
     path: ['confirmPassword'],
 });
+
+
+//cart Schemas
+export const cartItemSchema = z.object({
+    productId: z.string().min(1, 'Product is required'),
+    name: z.string().min(1, 'Name is required'),
+    slug: z.string().min(1, 'Slug is required'),
+    qty: z.number().int().nonnegative('Quantity must be a positive number'),
+    image: z.string().min(1, 'Image is required'),
+    price: currency,
+});
+
+export const insertCartSchema = z.object({
+    items: z.array(cartItemSchema),
+    itemsPrice: currency,
+    totalPrice: currency,
+    shippingPrice: currency,
+    taxPrice: currency,
+    sessionCartId: z.string().min(1, 'Session cart id is equired'),
+    userId: z.string().optional().nullable(),
+});
--- types/index.ts
+++ types/index.ts
@@ -1,9 +1,13 @@
 import {z} from 'zod';
-import { insertProductScheme } from '@/lib/validators';
+import { insertProductScheme, insertCartSchema, cartItemSchema } from '@/lib/validators';
 
 export type Product = z.infer<typeof insertProductScheme> & {
     id: string;
     rating: string;
     createdAt: Date;
     
-}
+};
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
+
