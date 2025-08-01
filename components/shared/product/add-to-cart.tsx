9d369a2b284b05cd9e763e2ac9b929f41ba2187b
Michelle Argueta <michelleargueta401@gmail.com>
2025-08-01 08:27:46 -0600

    shopping cart

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,45 +2,56 @@
 
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, Loader } from "lucide-react";
 import { Cart, CartItem } from "@/types";
 import { toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart-actions";
-
+import { useTransition } from "react";
 
 const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
   const router = useRouter();
 
+  const [isPending, startTransition] = useTransition();
+
+
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
 
-    if (!res.success) {
-      toast.error(res.message); // puedes usar toast.error o toast(<div>...</div>) según el diseño
-      return;
-    }
+    startTransition(async ()=>{
+      const res = await addItemToCart(item);
 
-    toast(res.message, {
-      action: {
-        label: "Go to Cart",
-        onClick: () => router.push('/cart'),
-      },
+      if (!res.success) {
+        toast.error(res.message); // puedes usar toast.error o toast(<div>...</div>) según el diseño
+        return;
+      }
+      toast(res.message, {
+        action: {
+          label: "Go to Cart",
+          onClick: () => router.push('/cart'),
+        },
+      });
+
+
     });
+    
 
     
   };
 
   //function handle remove from cart
   const handleRemoveFromCart = async() => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async ()=>{
+      const res = await removeItemFromCart(item.productId);
 
-    if (res.success) {
-      toast(res.message);
-    } else {
-      toast.error(res.message); // usa sonner.error para estilo de error
-    }
+      if (res.success) {
+        toast(res.message);
+      } else {
+        toast.error(res.message); // usa sonner.error para estilo de error
+      }
 
-    return;
-  }
+      return;
+    });
+
+  };
 
   //check if item is in cart
   const existItem = cart && cart.items.find((x)=> x.productId === item.productId);
@@ -49,16 +60,24 @@ const AddToCart = ({ cart, item }: { cart?: Cart, item: CartItem }) => {
   return  existItem ? (
     <div>
       <Button type="button" variant='outline' onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4"/>
+        {isPending ? (<Loader className="w-4 h-4 animate-spin" />) : (
+          <Minus className="h-4 w-4"/>
+        )}
+
       </Button>
       <span className="px-2">{existItem.qty}</span>
       <Button type="button" variant='outline' onClick={handleAddToCart}>
-        <Plus className="h-4 w-4"/>
+        {isPending ? (<Loader className="w-4 h-4 animate-spin" />) : (
+          <Plus className="h-4 w-4"/>
+        )}
       </Button>
     </div>
   ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus/>Add to Cart
+      {isPending ? (<Loader className="w-4 h-4 animate-spin" />) : (
+        <Plus className="h-4 w-4"/>
+      )} {''}
+      Add to Cart
     </Button>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -58,3 +58,22 @@ export function round2(value: number | string){
     throw new Error('Value is not a number or string')
   }
 }
+
+const CURRENCY_FORMATER = new Intl.NumberFormat('en-US', {
+  currency: 'USD',
+  style: 'currency',
+  minimumFractionDigits: 2
+});
+
+//format currency using formater above
+
+export function formatCurrency(amount: number | string | null) {
+  if (typeof amount === 'number'){
+    return CURRENCY_FORMATER.format(amount);
+  } else if (typeof amount === 'string'){
+    return CURRENCY_FORMATER.format(Number(amount));
+
+  } else{
+    return 'NaN';
+  }
+}
