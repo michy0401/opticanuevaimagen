ec91f0b8ade1a0ebe9a5e487adbc4b8687ea5224
Michelle Argueta <michelleargueta401@gmail.com>
2025-07-31 20:39:28 -0600

    delete from function

--- lib/actions/cart-actions.ts
+++ lib/actions/cart-actions.ts
@@ -7,6 +7,7 @@ import { prisma } from "@/db/prisma";
 import { cartItemSchema, insertCartSchema } from "../validators";
 import { revalidatePath } from "next/cache";
 import { Prisma } from "@prisma/client";
+import { success } from "zod";
 
 //calcualte cart prices
 
@@ -139,4 +140,56 @@ export async function getMyCart() {
             shippingPrice: cart.shippingPrice.toString(),
             taxPrice: cart.taxPrice.toString(),
         })
+}
+
+export async function removeItemFromCart(productId:string) {
+    try {
+        //check for cart cookie
+        const sessionCartId = (await cookies()).get('sessionCartId')?.value;
+        if(!sessionCartId) throw new Error('Cart session not found');
+
+        //get product
+        const product = await prisma.product.findFirst({
+            where: {id: productId}
+        });
+
+        if(!product) throw new Error('Product not found');
+
+        //get user cart
+        const cart = await getMyCart();
+        if (!cart) throw new Error('Cart not found');
+
+        //check for item
+        const exist = (cart.items as CartItem[]).find((x)=>x.productId===productId);
+        if (!exist) throw new Error('Item not found');
+
+        //check if only one in qty
+        if (exist.qty ===1) {
+            //remove from cart
+            cart.items =(cart.items as CartItem[]).filter((x)=>x.productId !== exist.productId);
+        }else{
+            //decrease qty
+            (cart.items as CartItem[]).find((x)=>x.productId === productId)!.qty =exist.qty - 1;
+
+        }
+
+        //update cart in data base
+        await prisma.cart.update({
+            where: {id: cart.id},
+            data: {
+                items: cart.items as Prisma.CartUpdateitemsInput[],
+                ...calcPrice(cart.items as CartItem[])
+            }
+        });
+
+        revalidatePath(`/product/${product.slug}`);
+
+        return {
+            success: true,
+            message: `${product.name} was removed from cart`
+        }
+
+    } catch (error) {
+        return{success: false, message: formatError(error)}
+    }
 }
